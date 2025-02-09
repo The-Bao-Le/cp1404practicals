765f846fe73a661eaf885861eca98326bf1373b6
The-Bao-Le <lethebao123456@gmail.com>
2025-02-09 12:13:30 +0800

    Refactor score evaluation program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,15 +2,27 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+import random
+def main():
     score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
 
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    grade = determine_grade(score)
+    print(grade)
+
+    random_score = random.randint(0, 100)
+    random_grade = determine_grade(random_score)
+    print(random_grade)
+
+def determine_grade(score):
+    if score >= 90:
+        grade = "Excellent"
+    elif score >= 50:
+        grade = "Passable"
+    else:
+        grade = "Bad"
+    return grade
+
+main()
