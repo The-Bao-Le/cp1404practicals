0309edaa18e96eac1ad45d1fb3064d2b6ecf414c
The-Bao-Le <lethebao123456@gmail.com>
2025-02-16 09:20:31 +0800

    Add answers as comments

---
+++ prac_03/random.py
@@ -0,0 +1,17 @@
+# What did you see on line 1?
+# A random integer
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number was 5 and the largest was 20
+#
+# What did you see on line 2?
+# A random integer with a difference of 2
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number was 3 and the largest was 9
+# Could line 2 have produced a 4?
+# No
+#
+# What did you see on line 3?
+# A random float value
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number was 2.5 and the largest was 5.5
+
