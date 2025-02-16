e28986e089d6414d82d2fcf95a9f3c7d8b506b46
The-Bao-Le <lethebao123456@gmail.com>
2025-02-16 14:28:39 +0800

    Add code for reading and printing file

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,3 +4,8 @@ out_file = open(FILENAME, 'w')
 name = input("Name: ")
 print(name, file=out_file)
 out_file.close()
+
+in_file = open(FILENAME, 'r')
+text = in_file.read()
+in_file.close()
+print(text)
