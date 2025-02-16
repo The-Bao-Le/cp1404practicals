95d1030ad66873e0f2c4728ddb97bd3570f57dde
The-Bao-Le <lethebao123456@gmail.com>
2025-02-16 09:44:48 +0800

    Add answers to questions in comment

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,8 +2,11 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+When the input is not an integer.
 2. When will a ZeroDivisionError occur?
+When the denominator is zero.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+Yes
 """
 
 try:
