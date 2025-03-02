2c0492ceb7881d9026db476f67d7a4d795533ebf
The-Bao-Le <lethebao123456@gmail.com>
2025-03-02 12:03:12 +0800

    Add function to load record

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,3 +7,13 @@ def main():
     records = load_records(FILENAME)
     champion_to_count, countries = process_records(records)
     display_results(champion_to_count, countries)
+
+
+def load_records(filename):
+    """Load data from a CSV file."""
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
