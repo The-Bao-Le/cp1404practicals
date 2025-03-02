180d38dcdf5827e21bab1d3c15983edd4a954ea1
The-Bao-Le <lethebao123456@gmail.com>
2025-03-02 11:30:23 +0800

    Add function to extract name from email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,7 +8,7 @@ def main():
     email_to_name = {}
     email = input("Email: ")
     while email != "":
-        name = input("Name: ")
+        name = get_name_from_email(email)
         confirmation = input(f"Is your name {name}? (Y/n) ")
         if confirmation.upper() != "Y" and confirmation != "":
             name = input("Name: ")
@@ -17,6 +17,14 @@ def main():
 
     for email, name in email_to_name.items():
         print(f"{name} ({email})")
+
+
+def get_name_from_email(email):
+    """Extract the name from email ID"""
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
 
 
 main()
