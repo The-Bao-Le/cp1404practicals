063fb797180ca0ff862551da210b74323c78b503
The-Bao-Le <lethebao123456@gmail.com>
2025-03-02 12:05:10 +0800

    Add code to load_records function to remove csv header

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -13,6 +13,7 @@ def load_records(filename):
     """Load data from a CSV file."""
     records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
         for line in in_file:
             parts = line.strip().split(",")
             records.append(parts)
