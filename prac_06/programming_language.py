a0369f0433a0e9174b703cf8f646d00455e0af97
The-Bao-Le <lethebao123456@gmail.com>
2025-03-12 14:33:03 +0800

    Finish ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,22 @@
+"""
+Programming Language
+Estimate time: 15 minutes
+Actual time: 10 minutes
+"""
+class ProgrammingLanguage:
+    """Represent a programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True if the language is dynamically typed, otherwise False."""
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Return a string representation of the programming language."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
