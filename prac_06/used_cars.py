3535a1720aafc4ec7a93d4012d4659a9fff8b966
The-Bao-Le <lethebao123456@gmail.com>
2025-03-12 14:17:20 +0800

    Finish the Modifications section

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,12 +9,14 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("Car",180)
     my_car.drive(30)
-    limo = Car(100)
+    limo = Car("Limo",100)
+    limo.add_fuel(20)
+    limo.drive(115)
     print(f"Limo has fuel: {limo.fuel}")
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
-
+    print(limo)
 
 main()
