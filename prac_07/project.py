09ad7679c1246c467dfb6dc4386b588beb8e6104
The-Bao-Le <lethebao123456@gmail.com>
2025-03-23 13:22:42 +0800

    Update class using datetime

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,19 +1,21 @@
+from datetime import datetime
+
 class Project:
     """Represent a project entity."""
 
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         """Initialize a Project instance."""
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y")
         self.priority = int(priority)
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = int(completion_percentage)
 
     def __str__(self):
         """Return a string representation of the project."""
-        return (f"{self.name}, Start Date: {self.start_date}, "
-                f"Priority: {self.priority}, Cost Estimate: ${self.cost_estimate:.2f}, "
-                f"Completion: {self.completion_percentage}%")
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:.2f}, "
+                f"completion: {self.completion_percentage}%")
 
     def __lt__(self, other):
         """Compare projects based on start date."""
